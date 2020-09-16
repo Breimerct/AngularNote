3d745573a63d9a7d4888ace68d4bb9638a4644b6
Breimerct <breimerct@gmail.com>
2020-09-16 18:42:08 -0500

    firebaseConfig para modo produccion

--- src/app/modules/firebase.module.ts
+++ src/app/modules/firebase.module.ts
@@ -1,4 +1,4 @@
-import { environment } from './../../environments/environment';
+import { environment } from './../../environments/environment.prod';
 import { NgModule } from '@angular/core'
 import { AngularFireModule } from 'angularfire2'
 import { AngularFirestoreModule } from 'angularfire2/firestore'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyC0wyxvlKODTpuiluR6m9grU6I44r539J4",
+    authDomain: "bct-angular-fire.firebaseapp.com",
+    databaseURL: "https://bct-angular-fire.firebaseio.com",
+    projectId: "bct-angular-fire",
+    storageBucket: "bct-angular-fire.appspot.com",
+    messagingSenderId: "456214117527",
+    appId: "1:456214117527:web:f3455ccb04cee001a67dd2"
+  }
 };
